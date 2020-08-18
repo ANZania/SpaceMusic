600bfac1e241c57cbaf77db9d6fe9b2fa63dd135
prostochlen <iwan.maksackov@yandex.ru>
2020-08-18 11:37:37 +0300

    all of master

--- js/main.js
+++ js/main.js
@@ -8,15 +8,40 @@ const main = document.querySelector('.main');
 const burger = document.querySelector('.burger');
 const chooseContainer = document.querySelector('.choose-container');
 const playListMusic = document.querySelector('.play-list-music');
-
-
-
+const favorite = document.querySelector('.favorite');
+const listFav = document.querySelector('.list-fav');
+const list = document.querySelector('.list');
+const nowPlaying = document.querySelector('.now-playing');
 
 btnStart.addEventListener('click', () => {
     container.classList.add('animate__fadeOutUp');
     choose.classList.add('animate__fadeInUp');
     choose.style.optical = '1';
     main.style.filter = 'brightness(40%)';
+});
+
+nowPlaying.addEventListener('click', () => {
+    favorite.classList.add('not-choosen');
+    nowPlaying.classList.remove('not-choosen');
+    listFav.classList.remove('animate__fadeInRight');
+    listFav.classList.add('animate__fadeOutRight');
+    list.classList.remove('animate__fadeOutRight');
+    list.classList.add('animate__fadeInRight');
+    list.style.display = 'block';
+    listFav.style.marginTop = '1000px';
+    listFav.style.optical = '0';
+});
+
+favorite.addEventListener('click', () => {
+    favorite.classList.remove('not-choosen');
+    nowPlaying.classList.add('not-choosen');
+    listFav.classList.remove('animate__fadeOutRight');
+    listFav.classList.add('animate__fadeInRight');
+    list.classList.remove('animate__fadeInRight');
+    list.classList.add('animate__fadeOutRight');
+    list.style.display = 'none';
+    listFav.style.marginTop = '0';
+    listFav.style.optical = '1';
 });
 
 burger.addEventListener('click', () => {
@@ -26,6 +51,7 @@ burger.addEventListener('click', () => {
         chooseContainer.classList.add('animate__fadeOutUp');
         playListMusic.classList.remove('animate__fadeOutUp');
         playListMusic.classList.add('animate__fadeInUp');
+        playListMusic.style.marginTop = '0';
         playListMusic.style.optical = '1';
         main.style.filter = 'brightness(20%)';
     } else {
--- js/musicPlayer.js
+++ js/musicPlayer.js
@@ -1,6 +1,6 @@
 export const musicPlayerInit = () => {
     const audioPlayer = document.querySelector('.audio-player');
-    //const playbtn = document.querySelector('.play');
+    const playbtn = document.querySelector('.play');
     //const prev = document.querySelector('.prev');
     //const next = document.querySelector('.next');
     const playerNav = document.querySelector('.player-nav');
@@ -10,37 +10,107 @@ export const musicPlayerInit = () => {
     const musicProgress = document.querySelector('.music-progress');
     const musicVolume = document.querySelector('.music-volume');
     const list = document.querySelector('.list');
+    const listFav = document.querySelector('.list-fav');
     const volume = document.querySelector('.volume');
-    
+    const maxStar = document.querySelector('.max');
+    let song;
+    let miniStar;
 
     const playlist = [
-    {name: 'Imagine Dragons - Beliver', duration: "03:24"}, 
-    {name: 'TFK - Move', duration: "03:28"}, 
-    {name: 'TFK - Oxygen', duration: "03:51"}, 
-    {name: 'grandson - Blood Water', duration: "03:36"}, 
-    {name: 'grandson - Identity', duration: "03:36"}, 
-    {name: 'grandson - thoughts prayers', duration: "03:45"}, 
-    {name: 'Linkin Park - Numb', duration: "03:05"}, 
-    {name: 'MORGENSHTERN - Пососи', duration: "02:10"}, 
-    {name: 'MORGENSHTERN - Yung Hefner', duration: "02:15"}, 
-    {name: 'MORGENSHTERN feat. Витя AK - РАТАТАТА', duration: "01:58"},
-    {name: 'NEFFEX - Numb', duration: "02:24"}, 
-    {name: 'Pyrokinesis - Чёрное солнышко', duration: "03:21"}, 
-    {name: 'Pyrokinesis -Корми демонов по рассписанию', duration: "03:59"}, 
-    {name: 'The Score - Born For This', duration: "03:54"}, 
-    {name: 'The Score - Miracle', duration: "03:26"}, 
-    {name: 'The Score - Stronger', duration: "03:10"}, 
-    {name: 'Twenty One Pilots - Chlorine', duration: "05:24"}, 
-    {name: 'Twenty One Pilots - Heathens', duration: "03:15"}];
+        {name: 'Imagine Dragons - Beliver', duration: "03:24", index: 0, fav: 'img/star-mini.svg'},
+        {name: 'Thousand Foot Krutch - Move', duration: "03:28", index: 1, fav: 'img/star-mini.svg'},
+        {name: 'Thousand Foot Krutch - Oxygen', duration: "03:51", index: 2, fav: 'img/star-mini.svg'},
+        {name: 'grandson - Blood Water', duration: "03:36", index: 3, fav: 'img/star-mini.svg'},
+        {name: 'grandson - Identity', duration: "03:36", index: 4, fav: 'img/star-mini.svg'},
+        {name: 'grandson - thoughts prayers', duration: "03:45", index: 5, fav: 'img/star-mini.svg'},
+        {name: 'Linkin Park - Numb', duration: "03:05", index: 6, fav: 'img/star-mini.svg'},
+        {name: 'MORGENSHTERN - Пососи', duration: "02:10", index: 7, fav: 'img/star-mini.svg'},
+        {name: 'MORGENSHTERN - Yung Hefner', duration: "02:15", index: 8, fav: 'img/star-mini.svg'},
+        {name: 'MORGENSHTERN feat. Витя AK - РАТАТАТА', duration: "01:58", index: 9, fav: 'img/star-mini.svg'},
+        {name: 'NEFFEX - Numb', duration: "02:24", index: 10, fav: 'img/star-mini.svg'},
+        {name: 'Pyrokinesis - Чёрное солнышко', duration: "03:21", index: 11, fav: 'img/star-mini.svg'},
+        {name: 'Pyrokinesis -Корми демонов по рассписанию', duration: "03:59", index: 12, fav: 'img/star-mini.svg'},
+        {name: 'The Score - Born For This', duration: "03:54", index: 13, fav: 'img/star-mini.svg'},
+        {name: 'The Score - Miracle', duration: "03:26", index: 14, fav: 'img/star-mini.svg'},
+        {name: 'The Score - Stronger', duration: "03:10", index: 15, fav: 'img/star-mini.svg'},
+        {name: 'Twenty One Pilots - Chlorine', duration: "05:24", index: 16, fav: 'img/star-mini.svg'},
+        {name: 'Twenty One Pilots - Heathens', duration: "03:15", index: 17, fav: 'img/star-mini.svg'}
+    ];
+
+    let playlistFav = JSON.parse(localStorage.getItem('key'));
+    console.log(playlistFav);
+    if (playlistFav != null){ 
+        playlistFav.forEach( (item) => {
+            listFav.insertAdjacentHTML('beforeend', `
+                <div class="song">
+                    <div class="song-name" value="name">${item.name}</div>
+                    <div class="star-and-time">
+                        <img src="${item.fav}" alt="" class="fav mini">
+                        <p class="music-time total">${item.duration}</p>
+                    </div>
+                </div>
+            `);
+        });
+    } else {
+        playlistFav = [];
+    }
+
+    playlist.forEach( i => {
+        list.insertAdjacentHTML('beforeend', `
+            <div class="song">
+                <div class="song-name" value="name">${i.name}</div>
+                <div class="star-and-time">
+                    <img src="${i.fav}" alt="" class="fav mini">
+                    <p class="music-time total">${i.duration}</p>
+                </div>
+            </div>
+        `);
+    });
+    miniStar = document.querySelectorAll('.mini');
+
     let trackIndex = 0;
 
+
     const addZero = n => n < 10 ? '0' + n : n;
+
+    const addToFav = item => {
+        playlistFav.push(item);
+        listFav.insertAdjacentHTML('beforeend', `
+            <div class="song">
+                <div class="song-name" value="name">${item.name}</div>
+                <div class="star-and-time">
+                    <img src="${item.fav}" alt="" class="fav mini">
+                    <p class="music-time total">${item.duration}</p>
+                </div>
+            </div>
+        `);
+        localStorage.setItem('key', JSON.stringify(playlistFav));
+    };
+
+    //const removeFromFav = item => {
+    //    playlistFav.push(item);
+    //    listFav.insertAdjacentHTML('beforeend', `
+    //        <div class="song">
+    //            <div class="song-name" value="name">${item.name}</div>
+    //            <div class="star-and-time">
+    //                <img src="${item.fav}" alt="" class="fav mini">
+    //                <p class="music-time total">${item.duration}</p>
+    //            </div>
+    //        </div>
+    //    `);
+    //};
 
     const loadTrack = () => {
         const isPlayed = audioPlayer.paused;
         const track = playlist[trackIndex];
 
         musicName.textContent = track.name;
+        song.forEach( i => {
+            if (i.classList.contains('active')){
+                i.classList.remove('active');
+            }
+        });
+        song[trackIndex].classList.add('active');
         audioPlayer.src = `./audio/${track.name}.mp3`;
 
         if (isPlayed) {
@@ -73,8 +143,11 @@ export const musicPlayerInit = () => {
         if (target.classList.contains('play')) {
             if (audioPlayer.paused) {
                 audioPlayer.play();
+                song[trackIndex].classList.add('active');
+                playbtn.src = 'img/pause.svg';
             } else {
                 audioPlayer.pause();
+                playbtn.src = 'img/play.svg';
             }
 
             const track = playlist[trackIndex];
@@ -123,31 +196,64 @@ export const musicPlayerInit = () => {
         audioPlayer.volume = musicVolume.value / 100;
     });
 
-    //volume.addEventListener('mouseover', () => {
-    //    console.log(1);
-    //    musicVolume.classList.add('animate__fadeInLeft');
-    //    musicVolume.style.optical = '1';
-    //});
-
-    //volume.addEventListener('mouseout', () => {
-    //    console.log(2);
-    //    musicVolume.classList.remove('animate__fadeInLeft');
-    //    musicVolume.class.add('animate__fadeOutRight');
-    //    musicVolume.style.optical = '0';
-    //});
-
     musicVolume.value = audioPlayer.volume * 100;
 
-    playlist.forEach((i) => {
-        list.insertAdjacentHTML('beforeend', `
-            <div class="song">
-                <div class="song-name">${i.name}</div>
-                <div class="star-and-time">
-                    <img src="img/star-mini.svg" alt="" class="fav">
-                    <p class="music-time total">${i.duration}</p>
-                </div>
-            </div>
-        `);
+    maxStar.addEventListener('click', () => {
+        if (maxStar.classList.contains('w')) {
+            maxStar.src = 'img/fav-ico.svg';
+            maxStar.classList.remove('w');
+            for(let i in miniStar) {
+                if (i == trackIndex) {
+                    console.log(2);
+                    miniStar[i].src = 'img/star-mini.svg';
+                    playlist[trackIndex].fav = 'img/star-mini.svg';
+                    //removeFromFav(playlist[trackIndex]);
+                }
+            }
+        } else {
+            maxStar.src = 'img/star-white.svg';
+            maxStar.classList.add('w');
+            for(let i in miniStar) {
+                if (i == trackIndex) {
+                    console.log(1);
+                    miniStar[i].src = 'img/star-mimi-white.svg';
+                    playlist[trackIndex].fav = 'img/star-mimi-white.svg';
+                    addToFav(playlist[trackIndex]);
+                }
+            }
+        }
+    });
+
+    song = document.querySelectorAll('.song')
+
+    document.addEventListener('click', event => {
+        if (event.target.closest('.song')) {
+            song.forEach( i => {
+                if (i.classList.contains('active')){
+                    i.classList.remove('active');
+                }
+            });
+            event.target.closest('.song').classList.toggle('active');
+        }
+    });
+
+    document.addEventListener('click', event => {
+        if (event.target.closest('.song-name')) {
+            playlist.forEach( track => {
+                if (track.name == event.target.textContent) {
+                    trackIndex = track.index;
+                    musicName.textContent = event.target.textContent;
+                    const isPlayed = audioPlayer.paused;
+                    audioPlayer.src = `./audio/${track.name}.mp3`;
+                    if (isPlayed) {
+                        audioPlayer.pause();
+                    } else {
+                        audioPlayer.play();
+                    }
+                }
+            });
+           
+        }
     });
 
 };
